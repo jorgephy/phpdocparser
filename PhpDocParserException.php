7aa51bc944b4385864c9a95e889a9066e73c301c
Jorge Phy <53174794+jorgephy@users.noreply.github.com>
2019-07-23 11:10:17 +0200

    Add files via upload

---
+++ PhpDocParserException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * PhpDocParserException file
+ * 
+ * @package \PhpDocParser
+ * @copyright Copyright (C) 2019 Jorge Phy Labs, Inc. All rights reserved.
+ * @author Jorge Phy <dev.jorgephy@gmail.com>
+ * @link https://github.com/jorgephy/phpdocparser
+ * @version SVN: v0.1
+ */
+ 
+namespace Phy;
+
+/**
+ * PhpDocParserException class
+ *
+ * @author Jorge Phy <dev.jorgephy@gmail.com>
+ * @package \PhpDocParser
+ */
+class PhpDocParserException extends \Exception
+{}
